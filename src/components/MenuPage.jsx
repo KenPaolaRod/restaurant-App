94430ecbadd73689694d631405c5c67e1e64259d
KenPaolaRod <kenpaorin@gmail.com>
2023-11-04 20:36:14 -0400

    more menu dishes added

--- src/components/MenuPage.jsx
+++ src/components/MenuPage.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header"
 import Footer from "./Footer"
-import MenuUI from "./MenuUI"
+import MenuUI from "./MenuUi"
 
 export default function MenuPage() {
   return (
--- src/components/menu.js
+++ src/components/menu.js
@@ -35,6 +35,51 @@ export const menu = [
     id: 4
   },
 
+  {
+    image: "/images/menu/mojarra-frita.png",
+    dishTitle: "Mojarra Frita",
+    dishDescription: "Deep-Fried Mojarra - A coastal delight: Crispy and tender fish served with fresh sides, a journey for your taste buds.",
+    price: "$23",
+    atl: "Mojarra Frita dish image",
+    id: 5
+  },
+
+  {
+    image: "/images/menu/picada-antioqueña.png",
+    dishTitle: "Picada Antioqueña",
+    dishDescription: "Antioqueña Platter - A delightful Colombian selection featuring sausages, pork belly, plantains, and more. A true taste of Colombia",
+    price: "$30",
+    atl: "Picada Antioqueña dish image",
+    id: 6
+  },
+
+  {
+    image: "/images/menu/salchipapas.png",
+    dishTitle: "Salchipapas",
+    dishDescription: "A delightful fusion of crispy fries and savory sausages, offering a satisfying and flavorful treat that will leave you craving for more",
+    price: "$12",
+    atl: "Salchipapas dish image",
+    id: 7
+  },
+
+  {
+    image: "/images/menu/Sanchocho.png",
+    dishTitle: "Sanchocho",
+    dishDescription: "Sanchocho - A hearty Colombian soup, brimming with flavors, featuring meat, potatoes, plantains, and corn. A taste of Colombian comfort.",
+    price: "$17",
+    atl: "Sanchocho dish image",
+    id: 8
+  },
+
+  {
+    image: "/images/menu/tamal-antioqueño.png",
+    dishTitle: "Tamal Antioqueño",
+    dishDescription: "Tamal Antioqueño - A traditional Antioquian tamale, a delicious blend of corn masa and seasoned pork, wrapped in a banana leaf.",
+    price: "$8",
+    atl: "Tamal Antioqueño dish image",
+    id: 9
+  }
+
 
 ]
 
